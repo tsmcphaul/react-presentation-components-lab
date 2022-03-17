97fb324da4667506fff17d354a3c30f172e586ac
Tammarra McPhaul <shaemc22@gmail.com>
2022-03-16 20:10:14 -0400

    commit tree c1f542585c018f4592cb1b3be5e48c4113689ca8

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponent Here
+
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+
+    state = {
+        mood: "happy"
+    }
+
+    handleClick = () => {
+        const newMood = this.state.mood === "happy" ? "sad" : "happy";
+        this.setState({ mood: newMood });
+      }
+    
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        );
+    }
+}
+
+export default SimpleComponent;
